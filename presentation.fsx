b6416a549113c01c50a0659ae77f9f4f44cb6598
Михаил Проказин <bearpro@outlook.com>
2020-11-08 23:27:28 +0300

    Исправлены алгоритмы вычисления ускорения и эффективности.

--- presentation.fsx
+++ presentation.fsx
@@ -1,74 +1,63 @@
 #!/usr/bin/fsi
 open System
 open System.IO
+
+#nowarn "46"
 let (^) f x = f x
 
 let multiThread, singleThread = "parallel", "single"
+
+let path = fsi.CommandLineArgs.[1]
+
+let formatFloat (tw:TextWriter) value =
+    (sprintf "%.2f" value)
+        .Replace('.', ',')
+    |> tw.Write 
 
 type Line =
   { Algorithm: string
     Processes: int
     Amount: int
     Time: float }
+
 let source = 
-    File.ReadAllLines("report.txt")
-    |> Seq.map ^ fun line -> 
+    let parseLine (line: string) =
         let items = line.Split(":")
         { Algorithm = items.[0]
           Processes = if items.[0] = multiThread then Convert.ToInt32 items.[1] else 1
           Amount = Convert.ToInt32 items.[2]
           Time = float ^ Convert.ToInt32 items.[3]}
+    
+    File.ReadAllLines(path)
+    |> Seq.map parseLine
+        
 
-let accelerationBy algorithm =
-    source
-    |> Seq.filter ^ fun {Algorithm = a} -> a = algorithm
-    |> Seq.groupBy ^ fun x -> x.Amount
-    |> Seq.sortBy ^ fun (k, _) -> k
-    |> Seq.map ^ fun (amount, items) -> amount, Seq.averageBy (fun x -> x.Time) items
-    |> Seq.windowed 2
-    |> Seq.map ^ fun [|aa, a; ab, b|] -> sprintf "%i - %i" aa ab, a - b
-    |> List.ofSeq
+let parallelResults = source |> Seq.where ^ fun line -> line.Algorithm = multiThread
+let linearResults = source |> Seq.where ^ fun line -> line.Algorithm = singleThread
 
-let formatAcceleration data =
-    for (where, howMuch) in data do
-        printfn "%s:%.2f" where howMuch
-// Приращение времени выполнения от количества данных
-
-// formatAcceleration ^ accelerationBy multiThread
-// formatAcceleration ^ accelerationBy singleThread
-
-type 'a TableRowValue =
-    | Header of string
-    | Value of 'a
+module Metrics = 
+    let efficiency acceleration (processes: int) : float = 
+        acceleration / (float processes)
+    let acceleration parallelTime linearTime : float = 
+        linearTime / parallelTime
 
 let accelerationByThreadCount =
-    source
-    |> Seq.filter ^ fun {Algorithm = a} -> a = multiThread
-    |> Seq.groupBy ^ fun x -> x.Processes, x.Amount
-    |> Seq.sortBy ^ fun (k, _) -> k
-    |> Seq.map ^ fun ((p, a), xs) -> p, a, (Seq.exactlyOne xs).Time
-    |> List.ofSeq
+    parallelResults
+    |> Seq.map (fun parallel -> 
+        let linear = linearResults |> Seq.find ^ fun x -> x.Amount = parallel.Amount
+        let acceleration = Metrics.acceleration parallel.Time linear.Time
+        let efficiency = Metrics.efficiency acceleration parallel.Processes
+        {| Processes = parallel.Processes
+           Acceleration = acceleration
+           Amount = linear.Amount
+           Efficiency = efficiency |}
+    )
+    
 
-let formatShit data = 
-    for (p, a, t) in data do
-        (sprintf "%i:%i:%.2f" p a t).Replace('.', ',')
-        |> printfn "%s"
+let formatAcceleration 
+    (data: seq<{| Acceleration: float; Amount: int; Efficiency: float; Processes: int |}>) =
+    printfn "Ускорение и эффективность"
+    for item in data do
+        printfn "%a:%d:%d:%a" formatFloat item.Acceleration item.Processes item.Amount formatFloat item.Efficiency
 
-//formatShit accelerationByThreadCount
-        
-let parallelAdvantage =
-    List.ofSeq source
-    // |> List.partition ^ fun x -> x.Algorithm = multiThread
-    |> List.groupBy ^ fun x -> x.Amount, x.Algorithm
-    |> List.map ^ fun ((a, alg), xs) -> a, alg, xs |> List.averageBy ^ fun x -> x.Time
-    |> List.groupBy ^ fun (a, alg, t) -> a
-    |> List.map ^ fun (a, xs) -> a, 
-                                 xs |> List.where (fun (a, alg, t) -> alg = multiThread) |> List.map (fun (a, alg, t) -> t) |> List.exactlyOne,
-                                 xs |> List.where (fun (a, alg, t) -> alg = singleThread) |> List.map (fun (a, alg, t) -> t) |> List.exactlyOne
-
-let formatAdvantage data =
-    for (amount, pt, lt) in data do
-        (sprintf "%i:%f:%f" amount pt lt).Replace('.', ',')
-        |> printfn "%s"
-
-formatAdvantage parallelAdvantage
+formatAcceleration accelerationByThreadCount
